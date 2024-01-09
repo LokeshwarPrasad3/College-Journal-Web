af417385bc992889c8550d3fac2502c1f2529e22
LokeshwarMERN <lokeshwar.prasad.cse@gmail.com>
2024-01-09 22:01:45 +0530

    fixed * route

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ const App = () => {
         <Route path="/review-policy" element={<ReviewPolicy />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/*" element={<HomePage />} />
       </Routes>
       <Footer />
     </>
