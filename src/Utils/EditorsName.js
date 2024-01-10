3327b1b709b07e35665512381ab7a7c7005e50b8
LokeshwarMERN <lokeshwar.prasad.cse@gmail.com>
2024-01-10 16:00:51 +0530

    fix some typo

--- src/Utils/EditorsName.js
+++ src/Utils/EditorsName.js
@@ -2,21 +2,21 @@
 export const editorsList =
     [
         {
-            name: "Dr Saket Rungta",
+            name: "Dr. Saket Rungta",
             designation: "Director",
             degree: "",
             college: "Sanjay Rungta Group of Institution",
             email: "saket.rungta@rungtacolleges.com",
         },
         {
-            name: "Dr Kuldeep Bansal",
+            name: "Dr. Kuldeep Bansal",
             designation: "Adjunct Professor",
             degree: "",
             college: "Abo Akademi University, Turku, Southwest Finland, Finland",
             email: "kuldeep.bansal@abo.fi",
         },
         {
-            name: "Dr.Pramod Kumar",
+            name: "Dr. Pramod Kumar",
             designation: "Senior Scientist",
             degree: "",
             college: "Institute of Lung Health and Immunity(LHI), Helmholtz Zentrum München- German Research Center for Environmental Health Ingolstädter Landstraße 1 · D - 85764 Neuherberg, Germany",
@@ -24,56 +24,56 @@ export const editorsList =
         },
         {
 
-            name: "Dr Shrikant Burje",
+            name: "Dr. Shrikant Burje",
             degree: "(M.E.PhD)",
             designation: "Professor & Director Academics",
             college: "Sanjay Rungta Group of Institutions Bhilai",
             email: "shrikant.burje@rungtacolleges.com"
         },
         {
-            name: "Dr.Rajesh Kumar Nema",
+            name: "Dr. Rajesh Kumar Nema",
             degree: "(M.Pharm, PhD)",
             designation: "Principal",
             college: "Rungta Institute of Pharmaceutical Sciences, Bhilai(Chhattisgarh) India",
             email: "rajeshKumar.nema@rungtacolleges.com"
         },
         {
-            name: "Dr.Ranjeeta Tiwari Mishra",
+            name: "Dr. Ranjeeta Tiwari Mishra",
             degree: "(MBA, UGC - NET, PhD., PGDRD)",
             designation: "Associate Professor Department of Management",
             college: "Sanjay Rungta Group of Institutions Bhilai",
             email: "ranjeetatiwari.mishra@rungtacolleges.com",
         },
         {
-            name: "Dr.Bhumika Chandrakar",
+            name: "Dr. Bhumika Chandrakar",
             degree: "(Pharm D, MBA)",
             designation: "Associate professor",
             college: "Rungta Institute of pharmaceutical sciences, Bhilai, Chhattisgarh, India .",
             email: "bhumika.chandrakar@rungtacolleges.com",
         },
         {
-            name: "Dr.Anurag Sharma",
+            name: "Dr. Anurag Sharma",
             degree: "M.Tech., Ph.D. (Computer Science & Engineering)",
             designation: "Principal",
             college: "RSR Rungta College of Engineering and Technology, Bhilai",
             email: "principal.rsrrcet@rungtacolleges.com",
         },
         {
-            name: "Dr.Ashish Kumar Tamrakar",
+            name: "Dr. Ashish Kumar Tamrakar",
             degree: "M.Sc(CS), M.Tech.(CSE), Ph.D. (Computer Science & Engineering)",
             designation: "Associate Professor,",
             college: "RSR Rungta College of Engineering and Technology, Bhilai",
             email: "ashish.tamrakar@rungtacolleges.com",
         },
         {
-            name: "Dr.Abhijeet Ganguly ",
+            name: "Dr. Abhijeet Ganguly ",
             degree: "M.Tech.(Production Engineering), Ph.D. (Mechanical Engineering)",
             designation: "Assistant Professor and Head Mechanical Engineering Department,",
             college: "RSR Rungta College of Engineering and Technology, Bhilai",
             email: "abhijeet.ganguly@rungtacolleges.com",
         },
         {
-            name: "Dr Tripti Agrawal jain",
+            name: "Dr. Tripti Agrawal jain",
             degree: "Msc PhD Chemistry",
             designation: "Principal",
             college: "Rungta college of science and technology Durg",
